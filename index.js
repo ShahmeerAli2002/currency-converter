30306e3f5f7e30bc9b24b87e7d9c915453a0652c
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-04-07 11:43:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+//currency converter
+console.log("Welcome to Currency Converter");
+console.log("----------------------------");
+console.log("Select your currency");
+console.log("----------------------------");
+console.log("USD = 1");
+console.log("INR = 75.57");
+console.log("GBP = 0.76");
+console.log("EUR= 0.91");
+console.log("PKR = 280");
+console.log("----------------------------");
+console.log("****************************");
+const currency = {
+    USD: 1,
+    INR: 75.57,
+    GBP: 0.76,
+    EUR: 0.91,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        choices: ["USD", "INR", "GBP", "EUR", "PKR"],
+        message: "Select your currency"
+    },
+    {
+        name: "to",
+        type: "list",
+        choices: ["USD", "INR", "GBP", "EUR", "PKR"],
+        message: "select your second currency"
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "ENTER YOUR AMOUNT"
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let result = (amount * fromAmount) / toAmount;
+console.log(`Your amount is ${result.toFixed()}`);
+console.log('"#""#"""#""""#""#"""#"""#"""#""""#""""#"""#"""#"""#"');
